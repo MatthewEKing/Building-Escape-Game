0cdececada5f1a49c1ff82a664b4923fbeda2394
Matt <matt.king1119@gmail.com>
2022-03-28 21:09:11 -0400

    Add files via upload

--- OpenDoor.cpp
+++ OpenDoor.cpp
@@ -20,12 +20,6 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FRotator CurrentRotation =  GetOwner()->GetActorRotation();
-	CurrentRotation.Yaw = 90.f;
-	//FRotator OpenDoor{ 0.f, 90.f, 0.f };
-
-	GetOwner()->SetActorRotation(CurrentRotation);
-
 
 }
 
@@ -35,6 +29,13 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+	float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	FRotator OpenDoor(0.f, 0.f, 0.f);
+	OpenDoor.Yaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2.f); 
+	GetOwner()->SetActorRotation(OpenDoor);
+
 }
 
